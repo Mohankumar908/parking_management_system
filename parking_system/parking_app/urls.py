1d48af71ab5115251c4130796a1bf393410e4edd
Mohankumar908 <mohanvkumar8866@gmail.com>
2025-10-23 19:02:36 +0530

    updated the dbsqlite and dashboard

--- parking_system/parking_app/urls.py
+++ parking_system/parking_app/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'parking_app' 
-
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'), 
-    path('create_pass/', views.create_parking_pass, name='create_parking_pass'),
-    path('entry/', views.vehicle_entry, name='vehicle_entry'),
-    path('exit/', views.vehicle_exit, name='vehicle_exit'),
+    path('', views.dashboard_view, name='dashboard'),
+    path('create-pass/', views.create_parking_pass, name='create_pass'),
+    path('vehicle-entry/', views.vehicle_entry, name='vehicle_entry'),
+    path('vehicle-exit/', views.vehicle_exit, name='vehicle_exit'),
 ]
